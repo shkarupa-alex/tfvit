f8f9838194a5794d33b5ebbbe7f251c08087e09d
Shkarupa Alex <shkarupa.alex@gmail.com>
2023-11-16 10:17:28 +0300

    Add masking to class token layer (for tfclip)

--- tfvit/tests/test_clstok.py
+++ tfvit/tests/test_clstok.py
@@ -1,4 +1,6 @@
+import numpy as np
 import tensorflow as tf
+from keras import layers
 from keras.src.testing_infra import test_combinations, test_utils
 from tfvit.clstok import AddClassToken, SplitClassToken
 from testing_utils import layer_multi_io_test
@@ -23,6 +25,17 @@ class TestAddClassToken(test_combinations.TestCase):
             expected_output_shape=[None, 15, 4],
             expected_output_dtype='float32'
         )
+
+    def test_mask(self):
+        inputs = np.array([[2, 6, 1, 0], [2, 0, 0, 0]]).astype('int32')
+        embeddings = layers.Embedding(8, 2, mask_zero=True)(inputs)
+        result = self.evaluate(embeddings._keras_mask)
+        self.assertAllEqual(result, inputs.astype('bool'))
+
+        embeddings = AddClassToken(num_registers=2)(embeddings)
+        expected = np.array([[1, 1, 1, 2, 6, 1, 0], [1, 1, 1, 2, 0, 0, 0]]).astype('bool')
+        result = self.evaluate(embeddings._keras_mask)
+        self.assertAllEqual(result, expected)
 
 
 @test_combinations.run_all_keras_modes
